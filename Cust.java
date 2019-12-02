81a0f1d76ce1de0e67bd93d73adfefb3d5b35da5
Abishajee <58020118+Abishajee@users.noreply.github.com>
2019-12-02 10:01:43 +0530

    Add files via upload

---
+++ Cust.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Cust {
+	int id;
+	String name,address,mob,mail,prooft,proofno;
+	void register(int id,String name,String address,String mob,String mail,String prooft,String proofno)
+	{
+	this.id=id;
+	this.name=name;
+	this.address=address;
+	this.mob=mob;
+	this.mail=mail;
+	this.prooft=prooft;
+	this.proofno=proofno;
+	}
+
+	void display()
+	{
+
+	System.out.println(id+"\t\t"+name);
+	}
+
+}
